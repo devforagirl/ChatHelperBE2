61897782aaa0c3c3e25c0e4c17ee4a7a3d323354
devforagirl <48156801+devforagirl@users.noreply.github.com>
2023-02-20 08:24:25 +0000

    updateVid + updateChatSpeed

--- api/index.py
+++ api/index.py
@@ -1,6 +1,42 @@
 from flask import Flask
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "secret!"
+socketio = SocketIO(app)
+
+currentVid = "86YLFOog4GM"
+
+chatSpeed = 5
+
+
+@socketio.on("updateChatSpeed")
+def handle_chatSpeed(seconds):
+    print("handle_chatSpeed->", seconds)
+
+    global chatSpeed
+    chatSpeed = seconds
+
+    fb = {"res_code": 1, "req_name": "updateChatSpeed", "chatSpeed": chatSpeed}
+
+    return fb
+
+
+@socketio.on("updateVid")
+def handle_updateVid(id):
+    print("handle_updateVid->", id)
+
+    global currentVid
+    currentVid = id
+
+    fb = {
+        "res_code": 1,
+        "req_name": "updateVid",
+        "currentVid": currentVid,
+        "note": "RESTART NEEDED",
+    }
+
+    return fb
 
 
 @app.route("/")
@@ -8,6 +44,5 @@ def home():
     return "Hello, World! 33345"
 
 
-@app.route("/about")
-def about():
-    return "About"
+if __name__ == "__main__":
+    socketio.run(app, host="127.0.0.1", port=5000, debug=True)

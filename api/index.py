39f25a8ba71bc15fc050d4ee979dd7a987a5e950
devforagirl <48156801+devforagirl@users.noreply.github.com>
2023-02-22 13:32:01 +0800

    update k
    k

--- api/index.py
+++ api/index.py
@@ -1,20 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify
 from flask_socketio import SocketIO
 from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-cors = CORS(app)
-socketio = SocketIO(app, cors_allowed_origins='https://rss2.searchx.tk')
+CORS(app)
+socketio = SocketIO(app)
 
 
 @app.route('/')
 def index():
-    return 'shou dao j'
+    return 'shou dao k'
+
 
 @socketio.on('connect')
 def test_connect():
     print('Client connected')
+
 
 @socketio.on('disconnect')
 def test_disconnect():
@@ -26,5 +28,6 @@ def handle_message(message):
     print('Received message: ' + message)
     socketio.emit('response', message)
 
+
 if __name__ == '__main__':
-    socketio.run(app)
+    app.run()

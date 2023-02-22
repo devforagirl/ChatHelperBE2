599f857e2141d0cb303e5c7ff4f57ccdf4b7f20c
devforagirl <48156801+devforagirl@users.noreply.github.com>
2023-02-22 14:12:12 +0800

    update o
    o

--- api/index.py
+++ api/index.py
@@ -3,12 +3,12 @@ from flask_socketio import SocketIO
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
+socketio = SocketIO(app, cors_allowed_origins="https://rss2.searchx.tk")
 
 
 @app.route('/')
 def index():
-    return 'shou dao m'
+    return 'shou dao o'
 
 
 @socketio.on('connect')
@@ -24,13 +24,14 @@ def test_disconnect():
 @socketio.on('message')
 def handle_message(message):
     print('Received message: ' + message)
-    socketio.emit('response', message)
 
-@app.after_request
-def add_cors_headers(response):
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
-    return response
+    counter = 0
+    while counter < 5:
+        socketio.emit('response')
+        counter += 1
+
+    # socketio.emit('response', message)
+    # socketio.emit('response')
 
 
 if __name__ == '__main__':
